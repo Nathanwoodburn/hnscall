14c6b61b0cb34f6aec08b929ebc34a201ed3d9e6
Nathan Woodburn <github@nathan.woodburn.au>
2023-05-04 19:15:55 +1000

    talk: remove all analytics script

--- public/js/umami.js
+++ public/js/umami.js
@@ -2,8 +2,9 @@
 
 // https://github.com/mikecao/umami
 
-const script = document.createElement('script');
-script.setAttribute('async', '');
-script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
-script.setAttribute('data-website-id', 'c7615aa7-ceec-464a-baba-54cb605d7261');
-document.head.appendChild(script);
+// const script = document.createElement('script');
+// script.setAttribute('async', '');
+// script.setAttribute('src', 'https://stats.mirotalk.com/script.js');
+// script.setAttribute('data-website-id', 'c7615aa7-ceec-464a-baba-54cb605d7261');
+// document.head.appendChild(script);
+console.log("No tracking for you!")
