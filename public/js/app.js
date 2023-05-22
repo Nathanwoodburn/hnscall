874d0213df20fcd087ac34c2a7d0a9fc0e4ca4bb
Nathan Woodburn <github@nathan.woodburn.au>
2023-05-22 19:56:50 +1000

    pwa: Edited JS path

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-          .register("/pwa.js")
+          .register("../js/pwa.js")
           .then(res => console.log("service worker registered"))
           .catch(err => console.log("service worker not registered", err))
       })
